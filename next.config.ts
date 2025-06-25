6e61bad1a12dd3d48c7f0f217110ca55021827ce
goosull <134674369+goosull@users.noreply.github.com>
2025-06-25 21:10:59 +0900

    feat: add API URL rewrites configuration

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,18 @@ const nextConfig: NextConfig = {
   experimental: {
     optimizePackageImports: ["@chakra-ui/react"],
   },
+
+  async rewrites() {
+    if (!process.env.API_URL) {
+      throw new Error("NEXT_PUBLIC_API_URL is not defined");
+    }
+    return [
+      {
+        source: "/api/:path*",
+        destination: `${process.env.API_URL}/:path*`,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
